3497f00ee5c3c9d337b978d263c70ddb8cd5ff1e
unrelentin <augmentedvisionjcd@gmail.com>
2024-04-04 21:32:12 +0700

    added hovering function

--- script.js
+++ script.js
@@ -9,12 +9,33 @@ function createGrid(squaresOfGrid) {
         const square = document.createElement("div");
             square.classList.add("gridBoxes");
             row.appendChild(square);
-        
-            board.appendChild(row);  //has to be in scope to reference                        
-        }                      
+            board.appendChild(row);  //has to be in scope to reference     
+        }  
+                            
     }
 }
 
 createGrid(16);
 
-  
+function coloringBook () {
+    const coloringEvent = document.querySelectorAll(".gridBoxes")
+    coloringEvent.forEach(div => {                                //important that it is for each
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = "black";
+        });
+    });
+};
+
+coloringBook();
+
+
+const newSketch = document.getElementById("newSketch");
+
+newSketch.addEventListener("click", () => {
+    let input = prompt("How many squares would you like per side in your new grid?",16);
+    if (input > 0 && input <= 100) {
+        board.replaceChildren();
+        createGrid(input);
+        coloringBook();
+    };
+    });
